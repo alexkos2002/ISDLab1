de43496847a1d8019be67b5fd456363d9644400c
Oleksii Kosiuk <oleksii_kosiuk@epam.com>
2022-09-27 04:44:34 +0300

    Add aircraft model and index.html.

---
+++ app/src/main/java/com/example/isdlab1/Aircraft.java
@@ -0,0 +1,31 @@
+package com.example.isdlab1;
+
+public class Aircraft {
+    private String name;
+    private double wingspan;
+    private int speed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWingspan() {
+        return wingspan;
+    }
+
+    public void setWingspan(double wingspan) {
+        this.wingspan = wingspan;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
