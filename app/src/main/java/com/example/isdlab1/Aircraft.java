817d0636c43972fc78b491860b65e46430b8b291
Oleksii Kosiuk <oleksii_kosiuk@epam.com>
2022-09-27 05:24:57 +0300

    Add ceiling field and constructors to aircraft model, set a text for TextView, update index.html.

--- app/src/main/java/com/example/isdlab1/Aircraft.java
+++ app/src/main/java/com/example/isdlab1/Aircraft.java
@@ -4,6 +4,17 @@ public class Aircraft {
     private String name;
     private double wingspan;
     private int speed;
+    private double ceiling;
+
+    public Aircraft(String name, double wingspan, int speed, double ceiling) {
+        this.name = name;
+        this.wingspan = wingspan;
+        this.speed = speed;
+        this.ceiling = ceiling;
+    }
+
+    public Aircraft() {
+    }
 
     public String getName() {
         return name;
@@ -27,5 +38,13 @@ public class Aircraft {
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    public double getCeiling() {
+        return ceiling;
+    }
+
+    public void setCeiling(double ceiling) {
+        this.ceiling = ceiling;
     }
 }
